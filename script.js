cd8394b72da9790208d723e6e3480a26b66e073e
ogasawarayui <ogasawara.yui@outlook.com>
2023-12-16 21:05:58 +0900

    js6

---
+++ script.js
@@ -0,0 +1,10 @@
+var text = "JavaScript"
+if (text == "JavaScript") { // 条件A
+  console.log("条件Aの処理");
+  console.log(text);
+} else if (text == "GitHub") { // 条件B
+  console.log("条件Bの処理");
+  console.log(text);
+} else { // 条件Aと条件Bに当てはまらなかった時の処理
+  console.log("条件Aと条件以外の処理");
+};
